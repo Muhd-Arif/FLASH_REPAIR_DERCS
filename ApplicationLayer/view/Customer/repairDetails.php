62eb6e5717542539254c8ef68e5c63907333bfdd
Muhd-Arif <muhdarif9805@gmail.com>
2021-06-04 22:07:49 +0800

    fix panow button

--- ApplicationLayer/view/Customer/repairDetails.php
+++ ApplicationLayer/view/Customer/repairDetails.php
@@ -29,7 +29,7 @@ $data = $repair->viewRepairDetails($RepairID);
     <title>Repair Details</title>
 
 
-        <!-- NAVIGATION BAR  -->
+    <!-- NAVIGATION BAR  -->
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -39,10 +39,10 @@ $data = $repair->viewRepairDetails($RepairID);
     <link rel="stylesheet" href="../../../assets/css/adminlte.min.css">
 
     <style>
-        .pay-now{
-            display: block;
-            text-transform: uppercase;
-        }
+    .pay-now {
+        display: block;
+        text-transform: uppercase;
+    }
     </style>
 </head>
 
@@ -62,7 +62,7 @@ $data = $repair->viewRepairDetails($RepairID);
 
 
 
-<div class="wrapper">
+    <div class="wrapper">
 
         <?php include("sidebar.php") ?>
 
@@ -70,84 +70,89 @@ $data = $repair->viewRepairDetails($RepairID);
         <div class="content-wrapper">
             <!-- NAVIGATION PART 1 STOP -->
 
-    <!-- REPAIR DETAILS -->
-    <?php
+            <!-- REPAIR DETAILS -->
+            <?php
             foreach($data as $row){
               $rpstatus = $row['RP_Status'];
             ?>
-    <div class="container">
-        <div class="order-wrapper <?php echo $rpstatus=='Cannot be repaired' ?'show-status':'hide-status';?>">
-            <div class="order-tracker">
-                <div class="incomplete">
-                    <i class="fa fa-ban" aria-hidden="true"></i>
-                    <div class="status">Cannot Be Repaired</div>
+            <div class="container">
+                <div class="order-wrapper <?php echo $rpstatus=='Cannot be repaired' ?'show-status':'hide-status';?>">
+                    <div class="order-tracker">
+                        <div class="incomplete">
+                            <i class="fa fa-ban" aria-hidden="true"></i>
+                            <div class="status">Cannot Be Repaired</div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
                 </div>
-                <div class="clear"></div>
-            </div>
-        </div>
 
-        <div class="order-wrapper <?php echo $rpstatus=='Cannot be repaired' ?'hide-status':'show-status';?>">
-            <div class="order-tracker">
-                <div class="complete pending">
-                    <i class="far fa-clock"></i>
-                    <div class="status">Pending</div>
-                    <div class="circle"></div>
+                <div class="order-wrapper <?php echo $rpstatus=='Cannot be repaired' ?'hide-status':'show-status';?>">
+                    <div class="order-tracker">
+                        <div class="complete pending">
+                            <i class="far fa-clock"></i>
+                            <div class="status">Pending</div>
+                            <div class="circle"></div>
+                        </div>
+                        <div
+                            class="<?php echo $rpstatus=='In Progress' || $rpstatus=='Completed' ?'complete':'';?> delivering">
+                            <i class="far fa-hourglass"></i>
+                            <div class="status">In Progress</div>
+                            <div class="circle"></div>
+                            <span class="line"></span>
+
+                        </div>
+                        <div class="<?php echo $rpstatus=='Completed'?'complete':'';?> delivered">
+                            <i class="far fa-check-circle"></i>
+                            <div class="status">Completed</div>
+                            <div class="circle"></div>
+                            <span class="line"></span>
+
+                        </div>
+                        <div class="clear"></div>
+                    </div>
                 </div>
-                <div class="<?php echo $rpstatus=='In Progress' || $rpstatus=='Completed' ?'complete':'';?> delivering">
-                    <i class="far fa-hourglass"></i>
-                    <div class="status">In Progress</div>
-                    <div class="circle"></div>
-                    <span class="line"></span>
+                <div class="row" style="justify-content: space-between;">
+                    <div class="col-md-4">
+                        <img src="<?="../../../uploads/".$row['RP_Image']?>" class="d-block w-100"
+                            onerror="this.src='../../../uploads/default.png';">
+                        <br><br>
+                        <?php if( $rpstatus =='Completed'){ ?>
+                        <a href="delivery.php?rpid=<?= $row['RP_ID']?>" class="pay-now btn btn-success">Pay Now</a>
+                        <?php }  ?>
 
+
+                    </div>
+
+
+
+                    <div class="col-md-7">
+
+                        <h2>Repair #<?=$row['RP_ID']?></h2>
+
+                        <p><b>Device Type: </b><?=$row['Q_DeviceType']?></p>
+                        <p><b>Damage: </b><?=$row['Q_DamageType']?></p>
+                        <p><b>Repair Status: </b><?=$row['RP_Status']?></p>
+                        <p><b>Reason: </b><?=$row['RP_Reason']?></p>
+                        <p><b>Quotation ID: </b><?=$row['Q_ID']?></p>
+                        <p><b>Quotation Date: </b><?=$row['Q_Date']?></p>
+                        <p><b>Estimated Cost: </b>RM<?=$row['Q_Cost']?></p>
+
+                    </div>
                 </div>
-                <div class="<?php echo $rpstatus=='Completed'?'complete':'';?> delivered">
-                    <i class="far fa-check-circle"></i>
-                    <div class="status">Completed</div>
-                    <div class="circle"></div>
-                    <span class="line"></span>
-
-                </div>
-                <div class="clear"></div>
             </div>
-        </div>
-        <div class="row" style="justify-content: space-between;">
-            <div class="col-md-4">
-                <img src="<?="../../../uploads/".$row['RP_Image']?>" class="d-block w-100"
-                    onerror="this.src='../../../uploads/default.png';">
-                <br><br>
-                <a href="delivery.php?rpid=<?= $row['RP_ID']?>" class="pay-now btn btn-success">Pay Now</a>
-            </div>
-
-
-
-            <div class="col-md-7">
-
-                <h2>Repair #<?=$row['RP_ID']?></h2>
-
-                <p><b>Device Type: </b><?=$row['Q_DeviceType']?></p>
-                <p><b>Damage: </b><?=$row['Q_DamageType']?></p>
-                <p><b>Repair Status: </b><?=$row['RP_Status']?></p>
-                <p><b>Reason: </b><?=$row['RP_Reason']?></p>
-                <p><b>Quotation ID: </b><?=$row['Q_ID']?></p>
-                <p><b>Quotation Date: </b><?=$row['Q_Date']?></p>
-                <p><b>Estimated Cost: </b>RM<?=$row['Q_Cost']?></p>
-
-            </div>
-        </div>
-    </div>
-    <?php } ?>
+            <?php } ?>
 
 
 
 
 
 
-    <!-- </div> -->
+            <!-- </div> -->
 
 
 
             <!-- NAVIGATION PART 2 -->
-            </div>
+        </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer no-print">
             <center><strong>Copyright &copy; 2022 Flash Repair</a>.</strong> All rights reserved. </center>
