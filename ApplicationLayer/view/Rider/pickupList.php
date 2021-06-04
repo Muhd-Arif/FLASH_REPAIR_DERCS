4cb546dbacc580fc24134f3cf6d3800abc6364a0
jenice11 <38397984+jenice11@users.noreply.github.com>
2021-06-04 22:05:50 +0800

    changed get rpid

--- ApplicationLayer/view/Rider/pickupList.php
+++ ApplicationLayer/view/Rider/pickupList.php
@@ -20,8 +20,8 @@ $data = $product->viewAllPickup();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $product->acceptPickup($RunnerID);
-  }
-    
+}
+
 
 ?>
 
@@ -34,13 +34,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
-        crossorigin="anonymous" />
+    crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <!-- NAVIGATION BAR  -->
     <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
     <link rel="stylesheet" href="../../../assets/css/adminlte.min.css">
     <!-- NAVIGATION BAR  -->
@@ -66,114 +66,114 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         border: 0;
         font-size: 1em;
     }
-    </style>
-    <!-- TEMPLATE 1  -->
-    <div class="wrapper">
-        <?php include("sidebar.php") ?>
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                
-                <!-- /.container-fluid -->
-            </section>
-            <section class="content">
-                <div class="container-fluid">
-                   <div class="row">
-                            <div class="col-12 ">
-                                <!-- Main content -->
+</style>
+<!-- TEMPLATE 1  -->
+<div class="wrapper">
+    <?php include("sidebar.php") ?>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            
+            <!-- /.container-fluid -->
+        </section>
+        <section class="content">
+            <div class="container-fluid">
+             <div class="row">
+                <div class="col-12 ">
+                    <!-- Main content -->
 
-                                <div class="card ">
-                                    <!-- /.col -->
-                                    <div class="container" >
-                                        <!-- TEMPLATE 1 STOP -->
-                                        <div class="card-header">
-                                            <h3 style="text-align: center">Available Pickup</h3>
-                                        </div>
-                                        <br>
-                                              
-                                                <!-- <input type="text" class="form-control mt-5" id="filterInput"
-            placeholder="Search quotation ID, device type, status"> -->
-
-                                                <!-- <table class="table mt-3" id="quotationList"> -->
-                                                <table class="table mt-5" id="dataTable">
-                                                    <thead class="thead-dark">
-                                                        <tr>
-                                                            <th scope="col">Quotation ID</th>
-                                                            <th scope="col">Customer Name</th>
-                                                            <th scope="col">Device Type</th>
-                                                            <th scope="col">Pickup Address</th>
-                                                            <th scope="col">SPickup Date</th>
-                                                            <th scope="col">Pickup Time</th>
-                                                            <th scope="col">Action</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                    <?php
-                        
-                        //foreach($data as $j => $value) {
-                          foreach($data as $row){
-                            $i = 1;
-                          ?> 
-
-                          <tr>
-                               
-                                <td><?=$row['Q_ID']?></td>
-                                <td><?=$row['C_Name']?></td>
-                                <td><?=$row['Q_DeviceType']?></td>
-                                <td><?=$row['P_Addr']?></td>
-                                <td><?=$row['P_Date']?></td>
-                                <td><?=$row['P_Time']?></td>
-                               <!--  <td><?=$row['Service']?></td> -->
-                              
-                                  <td>
-                                     <form id="delivery-form" method="post" >
-                                      <div id="parent" style="display:flex;">
-                                      <div class="" id="button1" style="margin-right:5px">
-                                          <input type="hidden" name="OrderProductID" value="<?=$row['P_ID']?>">
-                                           <input type="hidden" name="pickupAddress" value="<?=$row['P_Addr']?>">
-                                          <button id="show" type="submit" class="btn btn-primary accept" onclick="return confirm('Are you sure you want to accept this delivery?');">ACCEPT</button>
-                                        
-
-                                      </div>
-                                    
-                                    </div>
-                                    </form>
-                                  </td>
-
-                                 
-                                 </tr>
-                                
-                                <?php
-                              
-                            }
-                             
-                              ?>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                            <!-- TEMPLATE PART 2 -->
-                                        </div>
-                                    </div>
-                                </div>
+                    <div class="card ">
+                        <!-- /.col -->
+                        <div class="container" >
+                            <!-- TEMPLATE 1 STOP -->
+                            <div class="card-header">
+                                <h3 style="text-align: center">Available Pickup</h3>
                             </div>
-                        </div>
-                    </div>
-            </section>
-        </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer no-print">
-            <center><strong>Copyright &copy; 2022 Flash Repair</a>.</strong> All rights reserved. <center>
-        </footer>
-        <aside class="control-sidebar control-sidebar-dark">
-        </aside>
-    </div>
+                            <br>
+                            
+                                                <!-- <input type="text" class="form-control mt-5" id="filterInput"
+                                                    placeholder="Search quotation ID, device type, status"> -->
+
+                                                    <!-- <table class="table mt-3" id="quotationList"> -->
+                                                        <table class="table mt-5" id="dataTable">
+                                                            <thead class="thead-dark">
+                                                                <tr>
+                                                                    <th scope="col">Quotation ID</th>
+                                                                    <th scope="col">Customer Name</th>
+                                                                    <th scope="col">Device Type</th>
+                                                                    <th scope="col">Pickup Address</th>
+                                                                    <th scope="col">SPickup Date</th>
+                                                                    <th scope="col">Pickup Time</th>
+                                                                    <th scope="col">Action</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <?php
+                                                                
+                        //foreach($data as $j => $value) {
+                                                                foreach($data as $row){
+                                                                    $i = 1;
+                                                                    ?> 
+
+                                                                    <tr>
+                                                                     
+                                                                        <td><?=$row['Q_ID']?></td>
+                                                                        <td><?=$row['C_Name']?></td>
+                                                                        <td><?=$row['Q_DeviceType']?></td>
+                                                                        <td><?=$row['P_Addr']?></td>
+                                                                        <td><?=$row['P_Date']?></td>
+                                                                        <td><?=$row['P_Time']?></td>
+                                                                        <!--  <td><?=$row['Service']?></td> -->
+                                                                        
+                                                                        <td>
+                                                                           <form id="delivery-form" method="post" >
+                                                                              <div id="parent" style="display:flex;">
+                                                                                  <div class="" id="button1" style="margin-right:5px">
+                                                                                      <input type="hidden" name="OrderProductID" value="<?=$row['P_ID']?>">
+                                                                                      <input type="hidden" name="pickupAddress" value="<?=$row['P_Addr']?>">
+                                                                                      <button id="show" type="submit" class="btn btn-primary accept" onclick="return confirm('Are you sure you want to accept this delivery?');">ACCEPT</button>
+                                                                                      
+
+                                                                                  </div>
+                                                                                  
+                                                                              </div>
+                                                                          </form>
+                                                                      </td>
+
+                                                                      
+                                                                  </tr>
+                                                                  
+                                                                  <?php
+                                                                  
+                                                              }
+                                                              
+                                                              ?>
+                                                          </tbody>
+                                                      </table>
+                                                  </div>
+                                                  <!-- TEMPLATE PART 2 -->
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </section>
+                  </div>
+                  <!-- /.content-wrapper -->
+                  <footer class="main-footer no-print">
+                    <center><strong>Copyright &copy; 2022 Flash Repair</a>.</strong> All rights reserved. <center>
+                    </footer>
+                    <aside class="control-sidebar control-sidebar-dark">
+                    </aside>
+                </div>
     <!-- <script src="../../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
          <script src="../../../assets/js/adminlte.min.js"></script>
          <script src="../../../assets/js/demo.js"></script> -->
-    <!-- TEMPLATE PART 2 STOP -->
+         <!-- TEMPLATE PART 2 STOP -->
 
-    <script type="text/javascript">
+         <script type="text/javascript">
     // // Get input element
     // let filterInput = document.getElementById('filterInput');
     // // Add event listener
@@ -202,23 +202,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         });
     });
-    </script>
+</script>
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js" crossorigin="anonymous">
+</script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 </body>
 
 
